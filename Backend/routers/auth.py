d5de842b83a4105f1d7e1e045732b5bfe11542f0
RemyAde <remysamade@gmail.com>
2024-05-13 16:44:48 +0100

    add user login

--- Backend/routers/auth.py
+++ Backend/routers/auth.py
@@ -4,7 +4,10 @@ sys.path.append("..")
 
 from fastapi import APIRouter, Request, HTTPException, status
 from models import *
-from routers_utils.auth_functions import get_hashed_password, verify_token
+
+# Authentication
+from routers_utils.auth_functions import *
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 
 # signals
 from tortoise.signals import post_save
@@ -23,6 +26,44 @@ router = APIRouter(
     prefix="/auth",
     tags=["/auth"]
 )
+
+oauth_schema = OAuth2PasswordBearer(tokenUrl="auth/token")
+
+
+@router.post("/token")
+async def generate_token(request_form: OAuth2PasswordRequestForm = Depends()):
+    token = await token_generator(request_form.username, request_form.password)
+    return {"access_token": token, "token_type": "bearer"}
+
+
+async def get_current_user(token: str = Depends(oauth_schema)):
+    try:
+        payload = jwt.decode(token, config_credentials["secret_key"], algorithms=config_credentials["algorithm"])
+        user = User.get(id = payload.get("id"))
+    except:
+        raise(
+            HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Invalid user credentials",
+                headers={"WWW-Authenicate": "Bearer"}
+            )
+        )
+    
+    return await user
+
+
+@router.post("/me")
+async def user_login(user: user_pydanticIn = Depends(get_current_user)): # type: ignore
+    return {
+        "status": "ok",
+        "data": {
+            "username": user.username,
+            "email": user.email,
+            "verified": user.is_verified,
+            "joined_date": user.join_date.strftime("%b %d %Y")
+        }
+    }
+
 
 @post_save(User)
 async def send_verfication_email(
